8458984533f7fbee0a007cafb053ed2df88a27ad
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-02-05 16:22:38 +0530

    Time: 7 ms (10.26%) | Memory: 46.8 MB (68.48%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,18 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-
-            if (fast == slow) {
-                return true;
-            }
+        HashSet<ListNode> set=new HashSet<>();
+        ListNode temp=head;        
+        while(temp!=null){
+            if(set.contains(temp)) return true;
+            else set.add(temp);
+            temp=temp.next;
         }
-
-        return false;        
+        return false;
     }
+    
 }
