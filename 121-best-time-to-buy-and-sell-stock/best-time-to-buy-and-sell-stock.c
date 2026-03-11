ecfcebf285c374cb59bcbea1a07032b6608cbe1b
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-03-11 10:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (6.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -1,5 +1,6 @@
 int maxProfit(int* prices, int n) {
     int cp=0,mp=0,mbp=prices[0];
+    //fix the array first nums as minimum buy price if the other num is less than the mbp then update mbp  which is called buy the stock or else find the cp which means sell the stock if the cp is greater than mp update the cp and finally return the mp 
     for(int i=1;i<n;i++){
         if(prices[i]<mbp)mbp=prices[i];
         else{
