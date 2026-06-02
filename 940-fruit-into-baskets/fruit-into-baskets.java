a0a60afced7680994312371fffb2f44666eee8f4
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-02 09:32:24 +0530

    Time: 53 ms (81.25%) | Memory: 70.6 MB (40.23%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,12 +1,12 @@
 class Solution {
     public int totalFruit(int[] f) {
         int l=0,maxLen=0;
-        HashMap<Integer,Integer> mp=new HashMap<>();
+        HashMap<Integer,Integer> mp=new HashMap<>();//creating a hashmap
         for(int r=0;r<f.length;r++){
-            mp.put(f[r],mp.getOrDefault(f[r],0)+1);
-            while(mp.size()>2){
+            mp.put(f[r],mp.getOrDefault(f[r],0)+1);//add the element into hashmap 
+            while(mp.size()>2){//check the size of hashmap it is greater than two which means it has more than two distinct elements shrink the window and reduce its freq by 1
                 mp.put(f[l],mp.get(f[l])-1);
-                if(mp.get(f[l])==0){
+                if(mp.get(f[l])==0){// if the freq of the element is zero remove the element from hashmap
                     mp.remove(f[l]);
                 }
                 l++;
