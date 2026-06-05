e2dfa75ad7e7abeda985191290cf5311a46b7da2
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-05 12:11:22 +0530

    Time: 1 ms (99.95%) | Memory: 52.6 MB (76.81%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,37 +10,48 @@
  * }
  */
 public class Solution {
-    public int getlength(ListNode head){
-        int len=0;
-        ListNode temp=head;
-        while(temp!=null){
-            temp=temp.next;
-            len++;
-        }
-        return len;
-    }
+    // public int getlength(ListNode head){
+    //     int len=0;
+    //     ListNode temp=head;
+    //     while(temp!=null){
+    //         temp=temp.next;
+    //         len++;
+    //     }
+    //     return len;
+    // }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //s1->lena and lenb : diff = lenA-lenB
-        int lenA=getlength(headA);
-        int lenB=getlength(headB);
-        int diff= lenA-lenB;
-        //if diff is positive you have to move the a pointer if negative you have to move the b pointer 
-        ListNode tempA=headA, tempB=headB;
-        if(diff>0){
-            while(diff>0){
-                tempA=tempA.next;
-                diff--;
-            }
-        }else{
-            while(diff<0){
-                tempB=tempB.next;
-                diff++;
-            }
-        }
-        while(tempA!=tempB){
-            tempA=tempA.next;
-            tempB=tempB.next;
-        }
-        return tempA;
+        // //s1->lena and lenb : diff = lenA-lenB
+        // int lenA=getlength(headA);
+        // int lenB=getlength(headB);
+        // int diff= lenA-lenB;
+        // //if diff is positive you have to move the a pointer if negative you have to move the b pointer 
+        // ListNode tempA=headA, tempB=headB;
+        // if(diff>0){
+        //     while(diff>0){
+        //         tempA=tempA.next;
+        //         diff--;
+        //     }
+        // }else{
+        //     while(diff<0){
+        //         tempB=tempB.next;
+        //         diff++;
+        //     }
+        // }
+        // while(tempA!=tempB){
+        //     tempA=tempA.next;
+        //     tempB=tempB.next;
+        // }
+        // return tempA;
+
+
+
+
+    ListNode A=headA, B=headB;
+    if(A==null || B==null) return null;
+    while(A!=B){
+        A=(A==null) ? headB:A.next;
+        B = (B==null )? headA:B.next;
+    }
+    return A;
     }
 }
