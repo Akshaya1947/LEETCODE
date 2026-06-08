2b42763d5328c4a8b3cb60b3bee932863e4ff34a
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-08 09:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.21%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -11,7 +11,7 @@ class Solution {
         int n=nums.length;
         int []dp=new int[n];
         Arrays.fill(dp,-1);
-        int ans=f(n-1,nums,dp);
+        int ans=f(n-1,nums,dp);//last la irutnhu start aagum
         return ans;
     }
 }
