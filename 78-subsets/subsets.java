5431565c28d60ee1d9e602c88339db5b4495adbb
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-10 10:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (56.59%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    void f(int i,List<Integer> temp,int [] nums,List<List<Integer>> ans){
+        if(i==nums.length){
+            ans.add(new ArrayList<>(temp));//here temp ah staright add pan mudiyathu because so object create panni add panrom
+            return ;//to return back.backtrack
+        }
+        //notpick
+        f(i+1,temp,nums,ans);
+        //pick
+        temp.add(nums[i]);
+        f(i+1,temp,nums,ans);
+        temp.remove(temp.size()-1);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+             List<List<Integer>>ans =new ArrayList<>();
+         List<Integer>temp=new ArrayList<>();//we are using list data structure
+        f(0,temp,nums,ans);
+        return ans;
+    
+    }
+}
