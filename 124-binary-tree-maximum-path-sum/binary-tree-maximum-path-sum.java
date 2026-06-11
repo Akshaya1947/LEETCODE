fe8ded4121699b0eb84aebf57ee15c96b0b85fa8
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-11 16:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (55.42%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -13,24 +13,47 @@
  *     }
  * }
  */
-class Solution {
-    int maxi=Integer.MIN_VALUE;
-    int f(TreeNode root){
-      //if all node is negative return min negative value which will be max among all nodes
-      //even a single node is also considered
+// class Solution {
+//     int maxi=Integer.MIN_VALUE;
+//     int f(TreeNode root){
+//       //if all node is negative return min negative value which will be max among all nodes
+//       //even a single node is also considered
       
-        if(root==null)return 0;
-        int ls=f(root.left);
-        int rs=f(root.right);
-        if(ls<0)ls=0;//if it is negative make it as zero
-        if(rs<0)rs=0;
-        int sum=ls+rs+root.val;
-        maxi=Math.max(maxi,sum);
-        return Math.max(ls,rs)+root.val;
-    }
+//         if(root==null)return 0;
+//         int ls=f(root.left);
+//         int rs=f(root.right);
+//         if(ls<0)ls=0;//if it is negative make it as zero
+//         if(rs<0)rs=0;
+//         int sum=ls+rs+root.val;
+//         maxi=Math.max(maxi,sum);
+//         return Math.max(ls,rs)+root.val;
+//     }
 
+//     public int maxPathSum(TreeNode root) {
+//         int ans=f(root);
+//         return maxi;
+//     }
+// }
+
+
+
+
+// if you are not declaring the maxi as global variable
+class Solution {
+    public int f(TreeNode root, int[] maxi){
+        if(root==null) return 0;
+        int ls=f(root.left,maxi);
+        int rs=f(root.right,maxi);
+        if(ls<0) ls=0;
+        if(rs<0) rs=0;
+        int curr=root.val+ls+rs;
+        maxi[0]=Math.max(maxi[0],curr);
+        return root.val+Math.max(ls,rs);
+    }
     public int maxPathSum(TreeNode root) {
-        int ans=f(root);
-        return maxi;
+        int[] maxi=new int[1];
+        maxi[0]=Integer.MIN_VALUE;
+        int h=f(root,maxi);
+        return maxi[0];
     }
 }
