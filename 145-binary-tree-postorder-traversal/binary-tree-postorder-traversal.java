8518cb5c5699dc8da964082b560f2098310f18ae
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-11 12:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (14.88%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -13,17 +13,33 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> list=new ArrayList<>();
+//         if(root==null)return list;
+//         helper(root,list);
+//         return list;
+//     }
+//     public void helper(TreeNode root,List<Integer>list){
+//         if(root==null)return;
+//         helper(root.left,list);
+//         helper(root.right,list);
+//         list.add(root.val);
+//     }
+// }
+
+
+
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        if(root==null)return list;
-        helper(root,list);
-        return list;
+    public void f(TreeNode root, List<Integer> ans){
+        if(root==null) return;
+        f(root.left,ans);//left
+        f(root.right,ans);//right
+        ans.add(root.val);//root
     }
-    public void helper(TreeNode root,List<Integer>list){
-        if(root==null)return;
-        helper(root.left,list);
-        helper(root.right,list);
-        list.add(root.val);
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans =new ArrayList<>();
+        f(root,ans);
+        return ans;
     }
 }
