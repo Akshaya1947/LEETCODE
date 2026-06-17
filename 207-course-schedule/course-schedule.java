0d45afe347133433781164ffbc7d5c6158dc151f
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-17 09:23:28 +0530

    Time: 6 ms (72.92%) | Memory: 47.1 MB (63.04%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canFinish(int n, int[][] p) {
+    //create a adj list and indegree array
+        int indegree[]= new int[n];
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int [] edge : p){
+            int a =edge[0],b= edge[1];
+            indegree[a]++;
+            adj.get(b).add(a);
+        }
+        //find topological sort
+        Queue<Integer>q= new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int[] ans = new int[n];
+        int i=0;
+        while(!q.isEmpty()){
+            int node =q.poll();
+           ans[i]=node;//i=n-1 i++ means i==n which means array filled so return tha true or else return the false
+            i++;//
+            //check the adjacent connections
+            for(int a: adj.get(node)){
+                indegree[a]--;
+                if(indegree[a]==0){
+                    q.offer(a);
+                }
+
+            }
+        }
+        return i==n? true: false;//if it is valid return tha true or else return the false which you cannot take any course
+    }
+}
