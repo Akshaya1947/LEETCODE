0a776f9a27dc89ad2fdac2672c341190cab730c6
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-21 22:30:13 +0530

    Time: 4 ms (10.22%) | Memory: 50.3 MB (5.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -2,26 +2,25 @@ class Solution {
     int f(int i,int buy,int []p,int [][]dp){
         if(i>=p.length)return 0;
         if(dp[i][buy]!=-1)return dp[i][buy];
-        int profit=0;
-        if(buy==1){//you are eligible to buy
+        int pr=0;
+        if(buy==1){
             int bought=-p[i]+f(i+1,0,p,dp);
-            int skip=0+f(i+1,1,p,dp);//you can skip
-            profit+=Math.max(skip,bought);
+            int skip=0+f(i+1,1,p,dp);
+            pr+=Math.max(bought,skip);
         }else{
-            int sell=p[i]+f(i+1,1,p,dp);
-            int skip=0+f(i+1,0,p,dp);
-            profit+=Math.max(sell,skip);
+            int sell= p[i]+f(i+1,1,p,dp);
+            int skip=f(i+1,0,p,dp);
+            pr+=Math.max(sell,skip);
         }
-        return dp[i][buy]=profit;
+        return dp[i][buy]= pr;
     }
     public int maxProfit(int[] p) {
         int n=p.length;
-        int [][]dp=new int[n][2];//0 or 1 for buy so size 2
+        int [][]dp=new int[n][2];
         for(int []row:dp){
             Arrays.fill(row,-1);
         }
-        int ans=f(0,1,p,dp);//initially you are eligible to buy
+        int ans=f(0,1,p,dp);
         return ans;
-
     }
 }
