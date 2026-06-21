abb1c2af4cf777be89d03b6bfdc9746d1e6f9f50
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-21 21:41:51 +0530

    Time: 25 ms (28.15%) | Memory: 53.8 MB (68.24%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
-    int f(int i,int j,String s,String t,int [][]dp){
-        if(i<0||j<0)return 0;
+    int f(int i,int j,String t1,String t2,int [][]dp){
+        if(i<0 || j<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)){
-            return dp[i][j]=1+f(i-1,j-1,s,t,dp);//if common element present add 1 to answer and decrease both pointer
+        if(t1.charAt(i)==t2.charAt(j)){
+            return dp[i][j]=1+f(i-1,j-1,t1,t2,dp);
         }
-        //if no common you have two choice either you can move lfet or right
-        int left=0+f(i-1,j,s,t,dp);//if you move left decrease the i pointer
-        int right=0+f(i,j-1,s,t,dp);//if you move right you can move the j pointer 
+        int left=f(i-1,j,t1,t2,dp);
+        int right=f(i,j-1,t1,t2,dp);
         return dp[i][j]=Math.max(left,right);
     }
     public int longestCommonSubsequence(String t1, String t2) {
         int m=t1.length(),n=t2.length();
-        int [][]dp=new int[m][n];
+        int [][]dp = new int[m][n];
         for(int []row:dp){
             Arrays.fill(row,-1);
         }
         int ans=f(m-1,n-1,t1,t2,dp);
         return ans;
+        
     }
 }
