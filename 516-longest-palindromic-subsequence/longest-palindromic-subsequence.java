a33c6c8ed17b7b7dd57b5f18a204220237a4441c
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-21 21:53:07 +0530

    Time: 25 ms (89.73%) | Memory: 64.4 MB (9.23%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,22 +1,23 @@
 class Solution {
-    int f(int i,int j ,String s,int [][]dp){
-        if(i==j)return 1;//if both are in smae position return 1 which means you can element in middle
+    int f(int i,int j,String s,int[][]dp){
         if(i>j)return 0;
+        if(i==j)return 1;
         if(dp[i][j]!=-1)return dp[i][j];
         if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=2+f(i+1,j-1,s,dp);//here the both element is the part of palindrome for that adding 2 
+            return dp[i][j]=2+f(i+1,j-1,s,dp);
         }
         int left=f(i+1,j,s,dp);
         int right=f(i,j-1,s,dp);
         return dp[i][j]=Math.max(left,right);
     }
     public int longestPalindromeSubseq(String s) {
-        int m=s.length(),n=s.length();
-        int [][]dp =new int[m][m];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
-        }
-        int ans=f(0,m-1,s,dp);//i will start from zero and j will start from j
-        return ans;
+      int m=s.length(),n=s.length();
+      int [][]dp = new int[m][n];
+      for(int []row: dp){
+        Arrays.fill(row,-1);
+      }  
+      int ans=f(0,n-1,s,dp);
+      return ans;
+
     }
 }
