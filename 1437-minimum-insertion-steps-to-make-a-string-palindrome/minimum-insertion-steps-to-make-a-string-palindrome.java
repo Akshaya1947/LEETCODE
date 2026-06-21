e1db82f9df3ad79ab37ebb738e52bd140ee890ee
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-21 22:04:50 +0530

    Time: 22 ms (70.80%) | Memory: 46.6 MB (84.81%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,28 +1,27 @@
 class Solution {
-    int f(int i,int j ,String s,int [][]dp){
-        if(i==j)return 1;//if both are in smae position return 1 which means you can element in middle
+    int f(int i,int j, String s,int [][]dp){
+        if(i==j)return 1;
         if(i>j)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=2+f(i+1,j-1,s,dp);//here the both element is the part of palindrome for that adding 2 
+            return dp[i][j]=2+f(i+1,j-1,s,dp);
         }
         int left=f(i+1,j,s,dp);
-        int right=f(i,j-1,s,dp);
+        int right= f(i,j-1,s,dp);
         return dp[i][j]=Math.max(left,right);
     }
-    public int longestPalindromeSubseq(String s) {
+    public int longestpalindrome(String s){
         int m=s.length(),n=s.length();
-        int [][]dp =new int[m][m];
+        int [][]dp=new int[m][n];
         for(int []row:dp){
             Arrays.fill(row,-1);
         }
-        int ans=f(0,m-1,s,dp);//i will start from zero and j will start from j
+        int ans=f(0,n-1,s,dp);
         return ans;
     }
-
     public int minInsertions(String s) {
-        int lps=longestPalindromeSubseq( s);
-        int n=s.length();
-        return n-lps;//this will return the number of insertion that made to make the string  palindrome 
+        int n=longestpalindrome(s);
+        int m=s.length();
+        return m-n;
     }
 }
