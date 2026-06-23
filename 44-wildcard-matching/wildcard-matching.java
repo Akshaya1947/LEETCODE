d14a3a85d0061b2869dae7244544553e44b254fa
Akshaya1947 <akshayaa.24cse@kongu.edu>
2026-06-23 22:02:40 +0530

    Time: 87 ms (9.64%) | Memory: 63 MB (20.58%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,20 +1,23 @@
 class Solution {
     boolean f(int i,int j, String s,String p, int [][]dp){
+        //inga dp ah direct ah return panamudiyathu so ans nu bool la return panrom
         if(i==s.length()){
-            while(j<p.length() && p.charAt(j)=='*')j++;
+            while(j<p.length() && p.charAt(j)=='*')j++;//s=abd p=a*c ipo j++ pani next element iruka nu pakurom suppose irunthuchu na false ilana true (see down)
             if(j==p.length()) return true;
             return false;
         }
         boolean ans;
-        if(j==p.length())return false;
+        if(j==p.length())return false;// s=aa,p=a 
         if(dp[i][j]!=-1)return dp[i][j]==1;
         if(s.charAt(i)==p.charAt(j)){
-            ans=f(i+1,j+1,s,p,dp);
+            ans=f(i+1,j+1,s,p,dp);//same ah iruntha next element ku move panlam
         }
         else{
-            if(p.charAt(j)=='?') ans=f(i+1,j+1,s,p,dp);
+            if(p.charAt(j)=='?') ans=f(i+1,j+1,s,p,dp);//? iruntha single element ah replace panikalam
             else if(p.charAt(j)=='*')  ans = f(i+1,j,s,p,dp)|| f(i,j+1,s,p,dp);
-            else return ans=false;
+            // if suppose s=abd,p=a*c you have to return false because * replace b but d and c are not equal 
+            //if it was s=abd , p=a* return true; because * replace bd; 
+            else return ans=false;//not same and also no *, ? so return false
         }
          dp[i][j]= ans? 1:0;
         return ans;
