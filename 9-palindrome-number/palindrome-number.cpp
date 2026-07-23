76d9ed262df3e5971b25e4f5b72762389f2a2c01
Akshaya Alagesan <akshayaa.24cse@kongu.edu>
2026-07-23 20:55:31 +0530

    Time: 3 ms (44.46%) | Memory: 8.5 MB (65.64%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+     long long int rem,rev=0,original;
+    original=x;
+    if(x<0){
+        return false;
+    }
+    while(x!=0){
+        rem=x%10;
+        rev=rev*10+rem;
+        x=x/10;
+    }
+    if(original==rev){
+        return true;
+    }
+    return false;
+}
+};
