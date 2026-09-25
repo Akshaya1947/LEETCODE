77bd11366588cbbf83f7d75cf87a870fe39b49e2
Akshaya Alagesan <akshayaa.24cse@kongu.edu>
2026-09-25 09:17:49 +0530

    Time: 3 ms (89.35%) | Memory: 48.2 MB (76.71%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.java
+++ 495-teemo-attacking/teemo-attacking.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findPoisonedDuration(int[] t, int d) {
         int tot=0;
-        for(int i=0;i<t.length-1;i++){
-          tot+=Math.min(t[i+1]-t[i],d);
+        for(int i=0;i<t.length-1;i++){// 1 //for 4 the loop terminates 
+          tot+=Math.min(t[i+1]-t[i],d); //tot = 0+min(4-1,2) = 0+2 =2 
         }
-          if(t.length>0)tot+=d;
+          if(t.length>0)tot+=d;//2>0 tot = 2+2 =4
         return tot;
     }
 }
